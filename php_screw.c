08a85d4ea34da61fca1e796b0d012773c8ccbaf5
Böszörményi Zoltán <zboszor@pr.hu>
2020-01-29 06:40:56 +0100

    Rewrite as a Zend extension
    Code changes based on xdebug.
    Added initial PHP 7.4 support.
    Removed ancient PHP support with Zend API older than 20010901.
    
    Signed-off-by: Böszörményi Zoltán <zboszor@pr.hu>

--- php_screw.c
+++ php_screw.c
@@ -4,10 +4,15 @@
  * see file LICENSE for license details
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "php.h"
 #include "php_ini.h"
 #include "ext/standard/file.h"
 #include "ext/standard/info.h"
+#include "Zend/zend_extensions.h"
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -18,8 +23,14 @@
 #include "php_screw.h"
 #include "my_screw.h"
 
+#if PHP_VERSION_ID >= 70300
+static int (*php_screw_orig_post_startup_cb)(void);
+static int php_screw_post_startup(void);
+#endif
+
 PHP_MINIT_FUNCTION(php_screw);
 PHP_MSHUTDOWN_FUNCTION(php_screw);
+PHP_RINIT_FUNCTION(php_screw);
 PHP_MINFO_FUNCTION(php_screw);
 
 FILE *pm9screw_ext_fopen(FILE *fp)
@@ -98,24 +109,38 @@ ZEND_API zend_op_array *pm9screw_compile_file(zend_file_handle *file_handle, int
 	return org_compile_file(file_handle, type TSRMLS_CC);
 }
 
-zend_module_entry php_screw_module_entry = {
-#if ZEND_MODULE_API_NO >= 20010901
-	STANDARD_MODULE_HEADER,
+PHP_RINIT_FUNCTION(php_screw)
+{
+#if defined(ZTS) && defined(COMPILE_DL_PHP_SCREW) && defined(ZEND_TSRMLS_CACHE_UPDATE)
+	ZEND_TSRMLS_CACHE_UPDATE();
 #endif
-	"php_screw",
+
+#if PHP_VERSION_ID >= 70000
+	CG(compiler_options) = CG(compiler_options) | ZEND_COMPILE_EXTENDED_STMT;
+#endif
+
+	return SUCCESS;
+}
+
+zend_module_entry php_screw_module_entry = {
+	STANDARD_MODULE_HEADER,
+	PHPSCREW_NAME,
 	NULL,
 	PHP_MINIT(php_screw),
 	PHP_MSHUTDOWN(php_screw),
-	NULL,
+	PHP_RINIT(php_screw),
 	NULL,
 	PHP_MINFO(php_screw),
-#if ZEND_MODULE_API_NO >= 20010901
-	"1.5.0", /* Replace with version number for your extension */
-#endif
+	PHPSCREW_VERSION,
 	STANDARD_MODULE_PROPERTIES
 };
 
+#if COMPILE_DL_PHP_SCREW
 ZEND_GET_MODULE(php_screw);
+#if defined(ZTS) && defined(ZEND_TSRMLS_CACHE_DEFINE)
+ZEND_TSRMLS_CACHE_DEFINE();
+#endif
+#endif
 
 PHP_MINFO_FUNCTION(php_screw)
 {
@@ -126,16 +151,81 @@ PHP_MINFO_FUNCTION(php_screw)
 
 PHP_MINIT_FUNCTION(php_screw)
 {
-	// CG(extended_info) = 1;
+#if PHP_VERSION_ID < 70000
+	CG(compiler_options) = CG(compiler_options) | ZEND_COMPILE_EXTENDED_STMT;
+#endif
 
+	/*
+	 * Redirect compile function to our own. For PHP 7.3 and
+	 * later, we hook it in php_screw_post_startup instead.
+	 */
+#if PHP_VERSION_ID < 70300
 	org_compile_file = zend_compile_file;
 	zend_compile_file = pm9screw_compile_file;
+#endif
 	return SUCCESS;
 }
 
 PHP_MSHUTDOWN_FUNCTION(php_screw)
 {
-	// CG(extended_info) = 1;
 	zend_compile_file = org_compile_file;
 	return SUCCESS;
 }
+
+void php_screw_base_post_startup()
+{
+	org_compile_file = zend_compile_file;
+	zend_compile_file = pm9screw_compile_file;
+}
+
+ZEND_DLEXPORT int php_screw_zend_startup(zend_extension *extension)
+{
+#if PHP_VERSION_ID >= 70300
+	php_screw_orig_post_startup_cb = zend_post_startup_cb;
+	zend_post_startup_cb = php_screw_post_startup;
+	return zend_startup_module(&php_screw_module_entry);
+}
+
+static int php_screw_post_startup(void)
+{
+	if (php_screw_orig_post_startup_cb) {
+		int (*cb)(void) = php_screw_orig_post_startup_cb;
+
+		php_screw_orig_post_startup_cb = NULL;
+		if (cb() != SUCCESS) {
+			return FAILURE;
+		}
+	}
+
+	php_screw_base_post_startup();
+	return SUCCESS;
+#else
+	return zend_startup_module(&php_screw_module_entry);
+#endif
+}
+
+#ifndef ZEND_EXT_API
+#define ZEND_EXT_API	ZEND_DLEXPORT
+#endif
+
+ZEND_EXT_API zend_extension_version_info extension_version_info = { ZEND_EXTENSION_API_NO, (char*) ZEND_EXTENSION_BUILD_ID };
+
+ZEND_DLEXPORT zend_extension zend_extension_entry = {
+	(char*) PHPSCREW_NAME,
+	(char*) PHPSCREW_VERSION,
+	(char*) PHPSCREW_AUTHOR,
+	(char*) PHPSCREW_URL_FAQ,
+	(char*) PHPSCREW_COPYRIGHT_SHORT,
+	php_screw_zend_startup,
+	NULL,
+	NULL,			/* activate_func_t */
+	NULL,			/* deactivate_func_t */
+	NULL,			/* message_handler_func_t */
+	NULL,			/* op_array_handler_func_t */
+	NULL,			/* statement_handler_func_t */
+	NULL,			/* fcall_begin_handler_func_t */
+	NULL,			/* fcall_end_handler_func_t */
+	NULL,			/* op_array_ctor_func_t */
+	NULL,			/* op_array_dtor_func_t */
+	STANDARD_ZEND_EXTENSION_PROPERTIES
+};
--- php_screw.h
+++ php_screw.h
@@ -1,3 +1,13 @@
+#define PHPSCREW_NAME		"php_screw"
+#define PHPSCREW_VERSION	"1.6.0"
+#define PHPSCREW_AUTHOR		"Kunimasa Noda"
+#define PHPSCREW_URL_FAQ	"http://www.pm9.com"
+#define PHPSCREW_COPYRIGHT_SHORT	"(C) 2007 Kunimasa Noda/PM9.com, Inc."
+
+#if PHP_VERSION_ID < 70400
+#define ZEND_COMPILE_EXTENDED_STMT ZEND_COMPILE_EXTENDED_INFO
+#endif
+
 #define PM9SCREW        "\tPM9SCREW\t"
 #define PM9SCREW_LEN     10
 
