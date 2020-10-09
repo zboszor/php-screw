55c0f77ecd591726d251f15d0d28f59bf99a3c4f
Zoltán Böszörményi <zboszor@pr.hu>
2020-10-09 17:16:07 +0200

    Fix build error with PHP 7.4
    ZEND_HANDLE_FD doesn't exist in PHP 7.4.

--- php_screw.c
+++ php_screw.c
@@ -95,7 +95,9 @@ ZEND_API zend_op_array *pm9screw_compile_file(zend_file_handle *file_handle, int
 	}
 
 	if (file_handle->type == ZEND_HANDLE_FP) fclose(file_handle->handle.fp);
+#ifdef ZEND_HANDLE_FD
 	if (file_handle->type == ZEND_HANDLE_FD) close(file_handle->handle.fd);
+#endif
 	file_handle->handle.fp = pm9screw_ext_fopen(fp);
 	file_handle->type = ZEND_HANDLE_FP;
 
