b19d4fb1db5a16dc1a54443d8ab4e04462b00f0f
Zoltán Böszörményi <zboszor@gmail.com>
2025-06-24 11:50:08 +0200

    Use more sensible exit codes in screw.c and use remove/rename
    For actual errors, return 1 from screw.
    Use remove(3) and rename(2) instead of writing the original script.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- tools/screw.c
+++ tools/screw.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -19,12 +20,12 @@ int main(int argc, char **argv) {
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: filename.\n");
-		exit(0);
+		return 0;
 	}
 	fp = fopen(argv[1], "r");
 	if (fp == NULL) {
 		fprintf(stderr, "File not found(%s)\n", argv[1]);
-		exit(0);
+		return 1;
 	}
 
 	fstat(fileno(fp), &stat_buf);
@@ -35,18 +36,20 @@ int main(int argc, char **argv) {
 
 	if (memmem(datap, datalen, PM9SCREW, PM9SCREW_LEN) != NULL) {
 		fprintf(stderr, "Already Crypted(%s)\n", argv[1]);
-		exit(0);
+		return 0;
 	}
 
 	sprintf(oldfilename, "%s.screw", argv[1]);
 
-	fp = fopen(oldfilename, "w");
-	if (fp == NULL) {
-		fprintf(stderr, "Can not create backup file(%s)\n", oldfilename);
-		exit(0);
+	if (remove(oldfilename) != 0 && errno != ENOENT) {
+		fprintf(stderr, "Can not remove backup file(%s): %s\n", oldfilename, strerror(errno));
+		return 1;
 	}
-	fwrite(datap, datalen, 1, fp);
-	fclose(fp);
+
+	if (rename(argv[1], oldfilename) != 0) {
+		fprintf(stderr, "Can not create backup file(%s): %s\n", oldfilename, strerror(errno));
+		return 1;
+	}
 
 	newdatap = zencode(datap, datalen, &newdatalen);
 
@@ -56,7 +59,7 @@ int main(int argc, char **argv) {
 	fp = fopen(argv[1], "w");
 	if (fp == NULL) {
 		fprintf(stderr, "Can not create crypt file(%s)\n", oldfilename);
-		exit(0);
+		return 1;
 	}
 	fwrite(PM9SCREW, PM9SCREW_LEN, 1, fp);
 	fwrite(newdatap, newdatalen, 1, fp);
@@ -64,4 +67,6 @@ int main(int argc, char **argv) {
 	fprintf(stderr, "Success Crypting(%s)\n", argv[1]);
 	free(newdatap);
 	free(datap);
+
+	return 0;
 }
